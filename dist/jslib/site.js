bff5baea8cbc8acf862b094962ad916a505aac54
noahmistry <noah.mistry@gmail.com>
2019-03-25 23:18:20 -0700

    // home-slider, slick-slider css & js //

--- dist/jslib/site.js
+++ dist/jslib/site.js
@@ -1,23 +1,37 @@
+    $('.main-carousel').slick({
+        centerMode: true,
+        centerPadding: '160px',
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 1,
+        speed: 2000,
+        dots: false,
+        prevArrow: false,
+        nextArrow: false
+    });
 
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    // margin: 5,
-    // nav: true,
-    // animateIn:fadeIn,
-    dots:false,
-    autoplay: true,
-    // autoplayTimeout: 1000,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 1
-        },
-        1000: {
-            items: 2
-        }
-    }
-})
+
+
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     // margin: 5,
+//     // nav: true,
+//     // animateIn:fadeIn,
+//     dots:false,
+//     autoplay: true,
+//     // autoplayTimeout: 1000,
+//     autoplayHoverPause: true,
+//     responsive: {
+//         0: {
+//             items: 1
+//         },
+//         600: {
+//             items: 1
+//         },
+//         1000: {
+//             items: 2
+//         }
+//     }
+// })
 
