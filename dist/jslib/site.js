d33855b8302e4fe465c37612ec69b9ad74e2b831
Noah Mistry <noah.mistry@gmail.com>
2019-03-28 14:36:53 -0700

    // slick-variableWidth padding-left for images //

--- dist/jslib/site.js
+++ dist/jslib/site.js
@@ -1,37 +1,31 @@
     $('.main-carousel').slick({
         centerMode: true,
-        centerPadding: '160px',
-        adaptiveHeight: true,
+        variableWidth: true,
         autoplay: true,
         autoplaySpeed: 2000,
-        slidesToShow: 1,
+        slidesToShow: 2,
         speed: 2000,
         dots: false,
         prevArrow: false,
-        nextArrow: false
+        nextArrow: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
-
-
-
-// $('.owl-carousel').owlCarousel({
-//     loop: true,
-//     // margin: 5,
-//     // nav: true,
-//     // animateIn:fadeIn,
-//     dots:false,
-//     autoplay: true,
-//     // autoplayTimeout: 1000,
-//     autoplayHoverPause: true,
-//     responsive: {
-//         0: {
-//             items: 1
-//         },
-//         600: {
-//             items: 1
-//         },
-//         1000: {
-//             items: 2
-//         }
-//     }
-// })
-
