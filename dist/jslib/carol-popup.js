c18c3b0a92352fc1b22bc8ed51af08eec804af82
Noah Mistry <noah.mistry@gmail.com>
2019-03-30 18:33:10 -0700

    //magnific plugin - carol img gallery slider//

---
+++ dist/jslib/carol-popup.js
@@ -0,0 +1,88 @@
+$("#btn-popup-carol").magnificPopup({
+    items: [
+        {
+            src: "https://i.imgur.com/uaSKUbJ.jpg",
+            title: "Carol Gallery Image 1"
+        },
+        {
+            src: "https://i.imgur.com/mnUdIDw.jpg",
+            title: "Carol Gallery Image 2"
+        },
+        {
+            src: "https://i.imgur.com/R7lAs2t.jpg",
+            title: "Carol Gallery Image 3"
+        },
+        {
+            src: "https://i.imgur.com/mzgSWIq.jpg",
+            title: "Carol Gallery Image 4"
+        },
+        {
+            src: "https://i.imgur.com/Eh1XDea.jpg",
+            title: "Carol Gallery Image 5"
+        },
+        {
+            src: "https://i.imgur.com/SUGgGaV.jpg",
+            title: "Carol Gallery Image 6"
+        },
+        {
+            src: "https://i.imgur.com/aMWiwMG.jpg",
+            title: "Carol Gallery Image 7"
+        },
+        {
+            src: "https://i.imgur.com/aPukAah.jpg",
+            title: "Carol Gallery Image 8"
+        },
+        {
+            src: "https://i.imgur.com/yLICL6o.jpg",
+            title: "Carol Gallery Image 9"
+        },
+        {
+            src: "https://i.imgur.com/kudk6os.jpg",
+            title: "Carol Gallery Image 10"
+        },
+        {
+            src: "https://i.imgur.com/fvlyS51.jpg",
+            title: "Carol Gallery Image 11"
+        },
+        {
+            src: "https://i.imgur.com/PC7be4o.jpg",
+            title: "Carol Gallery Image 12"
+        },
+        {
+            src: "https://i.imgur.com/nJLje2D.jpg",
+            title: "Carol Gallery Image 13"
+        },
+        {
+            src: "https://i.imgur.com/VY1Ksco.jpg",
+            title: "Carol Gallery Image 14"
+        },
+        {
+            src: "https://i.imgur.com/GiTSSP1.jpg",
+            title: "Carol Gallery Image 15"
+        },
+        {
+            src: "https://i.imgur.com/E7zIAHc.jpg",
+            title: "Carol Gallery Image 16"
+        },
+        {
+            src: "https://i.imgur.com/cQ5lEN9.jpg",
+            title: "Carol Gallery Image 17"
+        },
+        {
+            src: "https://i.imgur.com/CkmwBrQ.jpg",
+            title: "Carol Gallery Image 18"
+        },
+        {
+            src: "https://i.imgur.com/cp8LTiN.jpg",
+            title: "Carol Gallery Image 19"
+        },
+        {
+            src: "https://i.imgur.com/o7nzXCW.jpg",
+            title: "Carol Gallery Image 20"
+        },
+    ],
+    gallery: {
+        enabled: true
+    },
+    type: "image" // this is a default type
+});
